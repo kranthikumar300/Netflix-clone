cf581bd73561413ed4ea1f31807e44c0c778a69d
kranthi <kranthikumar9.official@gmail.com>
2026-01-18 10:50:53 +0530

    Moved Utils folder to the src folder

--- src/Components/HeroPage.tsx
+++ src/Components/HeroPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { tmdbMovies, tmdbPopularMoviesUrl } from "./Utils/axiosInstance";
+import { tmdbMovies, tmdbPopularMoviesUrl } from "../Utils/axiosInstance";
 
 const HeroPage = () => {
 
